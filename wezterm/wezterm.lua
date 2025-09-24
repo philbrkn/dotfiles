ad903e3b6c5448cc37c38f20e89446f6fbb0a326
phil <phil@phils-macbook-air.tailf9c3e2.ts.net>
2025-09-24 15:04:45 -0400

    wezterm: tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,10 +6,47 @@ if wezterm.config_builder then
 end
 
 config.automatically_reload_config = true
-config.enable_tab_bar = false
+-- config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_tab_index_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+-- Add this function that was missing
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab_title(tab)
+	if tab.is_active then
+		return {
+			{ Text = "  " .. title .. "  " }, -- spaces for separation
+		}
+	end
+	if tab.is_last_active then
+		return {
+			{ Text = "      " .. title .. "*      " }, -- spaces for separation
+		}
+	end
+	-- Fix: Return a format table for inactive tabs too
+	return {
+		{ Text = "     " .. title .. "      " },
+	}
+end)
+
+config.tab_max_width = 50
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
-config.default_cursor_style = "BlinkingBar"
+config.default_cursor_style = "SteadyBlock"
 config.color_scheme = "Tokyo Night"
 -- config.window_background_opacity = 0.9
 -- config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
